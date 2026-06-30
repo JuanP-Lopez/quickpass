cb6494ad8c91711eb99573633055998ed7c44a1a
Juan Lopez <137343283+JuanP-Lopez@users.noreply.github.com>
2026-06-30 06:32:39 -0600

    Fetch a API en Railway

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -26,7 +26,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5111/api/auth/register", {
+      const res = await fetch("https://quickpassapi-production.up.railway.app/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
