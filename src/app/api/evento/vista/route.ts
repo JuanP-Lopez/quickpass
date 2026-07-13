f28545217d7fc65a73e73586504ab578cb35e2e4
Juan Lopez <137343283+JuanP-Lopez@users.noreply.github.com>
2026-07-12 19:08:39 -0600

    fix: ruta dinamica en route.ts

--- src/app/api/evento/vista/route.ts
+++ src/app/api/evento/vista/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, {params}: { params: Promise<{id: string}>}) {
+export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const id = searchParams.get("id");
 
